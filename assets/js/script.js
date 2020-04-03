a712da4abb9d1332400bce572949b16e4f1d7fe5
fiarrijal <52268836+fiarrijal@users.noreply.github.com>
2020-04-03 17:57:46 +0700

    Navbar responsive
    Js nya sudah benar. Tinggal CSSnya yang belum

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 
-let topnav = document.getElementsByClassName("topnav");
-let hamburger = document.getElementsById("hamburger");
+const topnav = document.querySelector(".topnav");
+const icon = document.getElementById("icon");
 
-hamburger.addEventListener("click", function() {
+icon.addEventListener("click", function() {
   topnav.classList.toggle("responsive");
-  return;
 });
 // document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
